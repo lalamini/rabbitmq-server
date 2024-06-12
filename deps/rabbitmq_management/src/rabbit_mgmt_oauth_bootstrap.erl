a273d4897f6862307f50691695eafd27d12df090
Marcial Rosales <marcial.rosales@broadcom.com>
2024-06-12 11:50:25 +0200

    Refactor  index.html
    Move out inline script that deals with
    oauth

--- deps/rabbitmq_management/src/rabbit_mgmt_oauth_bootstrap.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_oauth_bootstrap.erl
@@ -18,19 +18,23 @@ init(Req0, State) ->
      rabbit_mgmt_headers:set_common_permission_headers(Req0, ?MODULE), ?MODULE), State).
 
 bootstrap_oauth(Req0, State) ->
-  JSContent = oauth_initialize_if_required() ++ set_token_auth(Req0),
+  AuthSettings = rabbit_mgmt_wm_auth:authSettings(),
+  JSContent = case proplists:get_value(oauth_enabled, AuthSettings, false) of 
+    true -> oauth_initialize_if_required(AuthSettings) ++ set_token_auth(Req0) ++ oauth_initiate_if_required();
+    false -> oauth_initialize_if_required(AuthSettings)
+  end,
   {ok, cowboy_req:reply(200, #{<<"content-type">> => <<"text/javascript; charset=utf-8">>}, JSContent, Req0), State}.
 
-oauth_initialize_if_required() ->
+oauth_initialize_if_required(AuthSettings) ->
   ["function oauth_initialize_if_required() { return oauth_initialize(" ,
-    rabbit_json:encode(rabbit_mgmt_format:format_nulls(rabbit_mgmt_wm_auth:authSettings())) , ") }" ].
+    rabbit_json:encode(rabbit_mgmt_format:format_nulls(AuthSettings)) , ") }" ].
 
 set_token_auth(Req0) ->
-  case application:get_env(rabbitmq_management, oauth_enabled, false) of
-    true ->
-      case cowboy_req:parse_header(<<"authorization">>, Req0) of
-        {bearer, Token} ->  ["set_token_auth('", Token, "');"];
-        _ -> []
-      end;
-    false -> []
+  case cowboy_req:parse_header(<<"authorization">>, Req0) of
+    {bearer, Token} ->  ["set_token_auth('", Token, "');"];
+    _ -> []
   end.
+
+oauth_initiate_if_required() ->
+  ["var oauth = oauth_initialize_if_required(); oauth_initiate_if_required();"].
+
